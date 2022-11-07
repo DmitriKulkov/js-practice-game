e52becb035e8ba27088ce00d4c7d295209583d49
simplepc <simplepc@pop-os.localdomain>
2022-11-07 16:24:11 +0300

    feat(game): remake game concept

--- app.js
+++ app.js
@@ -1,29 +1,51 @@
 const board = document.querySelector('#board')
+const remove = document.querySelector('.reset')
 const colors = ['#EA047E', '#FF6D28', '#FCE700', '#00F5FF', '#D800A6']
-const SQUERES_NUMBER = 625
+const BOARD_WIDTH = 25
 
-for (let i = 0; i < SQUERES_NUMBER; i++){
-  const square = document.createElement('div')
-  square.classList.add('square')
-  square.addEventListener('mouseover', ()=>setColor(square))
-  square.addEventListener('mouseleave', ()=>removeColor(square))
+remove.addEventListener('click', removeColors)
 
-  board.append(square)
+for (let i = 0; i < BOARD_WIDTH; i++){
+  for (let j = 0; j < BOARD_WIDTH; j++){
+    const square = document.createElement('div')
+    square.setAttribute('id', `i_${i}_${j}`)
+    square.classList.add('square')
+    square.addEventListener('mouseover', ()=>setColor(square))
+    board.append(square)
+  }
 }
 
 function setColor(element){
   const color = getRandomColor()
+  const nearElements = getNearElements(element.id)
   element.style.backgroundColor = color
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+  nearElements.forEach((element)=>{
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+  })
 }
 
-function removeColor(element){
-  element.style.backgroundColor = '#1d1d1d'
-  element.style.boxShadow = `0 0 2px #000`
-
+function removeColors(){
+  const squares = document.querySelectorAll('.square')
+  squares.forEach((element)=>{
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+  })
 }
 
 function getRandomColor(){
   const index = Math.floor(Math.random() * colors.length)
   return colors[index]
+}
+
+function getNearElements(id){
+  const i_j = id.split('_')
+  const nearElements = document.querySelectorAll(
+    ((parseInt(i_j[1]) > 0? '#i_' + (parseInt(i_j[1])-1) + '_'+ i_j[2] + ',':'')+
+    (parseInt(i_j[2]) > 0? '#i_' + i_j[1] + '_' + (parseInt(i_j[2])-1) + ',':'')+
+    (parseInt(i_j[1]) < BOARD_WIDTH-1? '#i_' + (parseInt(i_j[1])+1) + '_'+ i_j[2] + ',':'')+
+    (parseInt(i_j[2]) < BOARD_WIDTH-1? '#i_' + i_j[1] + '_' + (parseInt(i_j[2])+1) + ',':'')).slice(0, -1)
+    )
+  return nearElements 
 }
